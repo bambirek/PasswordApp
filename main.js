e353365cf6471157a94842dc26e184f29a34a9fa
Maja Roksz <5009217@teb-gdansk.ad>
2024-01-27 12:02:43 +0100

    Password Application v1

---
+++ main.js
@@ -0,0 +1,19 @@
+// Pobrać inputa oraz diva
+
+// Zmienna globalna z hasłem
+// Zmienna globalna z tajną wiadomością
+
+// Nasłuchiwania na elemenencie input na event input - odczytać e.target.value, sprawdzić czy jest odpowiednie, pokazać wiadomość, potem ukryć wiadomość podczas pisania nowego hasła. Dodatkowo wyczyścić input po wpisaniu poprawnego hasła
+
+// Drugie nasłuchiwanie na elemencie input na event focus - moment kiedy input jest kliknięty. Wtedy ma mieć klasę active
+
+// Trzecie na elemencie input, na event blur - moment odklikania inputa io wtedy ma stracić klasę active.
+
+
+const input = document.querySelector('input');
+console.log(input.value);
+
+input.addEventListener('submit'), function(jakasFunckja) {
+  
+}
+
